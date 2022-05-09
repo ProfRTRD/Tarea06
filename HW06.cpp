da4651a50290f637ae9ac5add00e5bcb55029901
Juan <101371618+ProfRTRD@users.noreply.github.com>
2022-05-09 08:54:18 -0500

    Create HW06.cpp

---
+++ HW06.cpp
@@ -0,0 +1,18 @@
+template <typename func_t>
+double newton(double x0, double eps, func_t func, int nmax, int & nsteps)
+{
+  nsteps = 0;
+  double xr = x0;
+  while(nsteps <= nmax) {
+    if (std::fabs(func(xr)) < eps) {
+      break;
+    } else {
+      double h = 0.001;
+      double deriv = (func(xr+h/2) - func(xr-h/2))/h;
+      xr = xr - func(xr)/deriv;
+    }
+    nsteps++;
+  }
+
+  return xr;
+}
